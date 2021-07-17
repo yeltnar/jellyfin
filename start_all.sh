3e15509edd324fafad00647cde5a63776a37fcd1
Andrew Brantley <github@andbrant.com>
2021-07-17 13:10:31 -0500

    harder

--- start_all.sh
+++ start_all.sh
@@ -11,6 +11,15 @@ chown $(whoami) /tmp/$folder/ipfs
 chown $(whoami) /tmp/$folder/ipns
 
 export IPFS_PATH=$(pwd)/.ipfs
+
+printf 'make sure the daemon can find its self'
+ipfs daemon & 
+sleep 5
+echo 'rm' > "/tmp/$folder rm.txt"
+ipfs add "/tmp/$folder rm.txt"
+rm "/tmp/$folder rm.txt"
+ipfs shutdown 
+
 echo $IPFS_PATH
 ipfs config profile apply lowpower
 ipfs daemon --mount --mount-ipfs=/tmp/$folder/ipfs  --mount-ipns=/tmp/$folder/ipns &
