b45db74ccd1332a4d087d2a5321562a8d9cb115c
Andrew Brantley <github@andbrant.com>
2021-10-22 15:36:43 -0500

    fix rclone start

--- start_rclone.sh
+++ start_rclone.sh
@@ -4,6 +4,6 @@ sudo rm -rf /mnt/exposed-pi;
 sudo mkdir /mnt/exposed-pi;
 sudo chmod 777 /mnt/exposed-pi
 
-rclone mount --allow-non-empty --allow-other exposed-pi:/ /mnt/exposed-pi/ &
+rclone mount --vfs-cache-mode writes --allow-other exposed-pi:/ /mnt/exposed-pi/ &
 
 sleep 1;
