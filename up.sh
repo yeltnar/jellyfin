c4057b0ce195d290077c0a688a1596b148f8d1d1
Andrew Brantley <git@andbrant.com>
2025-02-21 10:52:05 -0600

    set up env file

--- up.sh
+++ up.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-#./start_rclone.sh 
-
-#if [[ ! -z $(ls /mnt/exposed-pi) ]]
-#then
-#    echo 'mounted; starting jellyfin';
-    docker-compose up --build -d 
-#else
-#    echo 'not mounted; exiting'
-#fi
-
+docker-compose --env-file .env up --build -d 
