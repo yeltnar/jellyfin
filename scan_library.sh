856a48d9821e27a0cfd28b986bb462b3332270e8
drew <nixosvm@andbrant.com>
2026-01-23 08:29:15 -0600

    add scan api call with sops

---
+++ scan_library.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source <(sops -d ./.env.enc)
+
+LIBRARY_ID="$MUSIC_LIBRARY_ID"
+
+curl -X POST "$SERVER_URL/Items/$LIBRARY_ID/Refresh" \
+     -H "Content-Type: application/json" \
+     -H "X-Emby-Token: $API_KEY" \
+     -d "{\"UserId\": \"$USER_ID\"}" \
+     -v
