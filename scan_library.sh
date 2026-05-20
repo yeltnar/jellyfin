77b38358872ca6e7d1419dd686eaf59664d9119e
drew <nixosvm@andbrant.com>
2026-05-20 10:30:15 -0500

    update mount to help with moving s3; update scan library

--- scan_library.sh
+++ scan_library.sh
@@ -7,5 +7,5 @@ LIBRARY_ID="$MUSIC_LIBRARY_ID"
 curl -X POST "$SERVER_URL/Items/$LIBRARY_ID/Refresh" \
      -H "Content-Type: application/json" \
      -H "X-Emby-Token: $API_KEY" \
-     -d "{\"UserId\": \"$USER_ID\"}" \
+     -d "{\"UserId\": \"$USER_NAME\"}" \
      -v
